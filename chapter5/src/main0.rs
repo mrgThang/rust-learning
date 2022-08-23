707adec3989b078bbd1979d3b989c2c7c2142461
Mabuu <phamducthang03102002@gmail.com>
2022-08-23 09:33:44 +0700

    Initialize chapter 5 and Define struct tutorial

---
+++ chapter5/src/main0.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1 // other fields are same as user1
+    };
+    // We are no longer use `user1` after create `user2`
+    // because `user1.username` was move to `user2.username`
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
